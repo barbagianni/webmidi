55bd27059d0367a8b9bcf9f38d45c2669b8de8d2
David <davidmm@gmail.com>
2014-06-13 10:13:44 +0200

    Rework to use promise instead of events; Optionally defining AMD; Remove unnecessary dependencies

--- Midi.js
+++ Midi.js
@@ -1,14 +1,19 @@
-define(['backbone', 'underscore'], function (Backbone, _) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.Midi = factory(root.b);
+    }
+})(this, function () {
     var Midi = function () {
+        this._actions = {};
         if (!navigator.requestMIDIAccess) {
-            console.error('Web MIDI not available!');
+            this._accessPromise = Promise.reject('Web MIDI not available!');
         } else {
-            this.actions = {};
-            var self = this;
-            navigator.requestMIDIAccess().then(
+            this._accessPromise = navigator.requestMIDIAccess().then(
                 function (midiAccess, midiOptions) {
-                    self.setMidiAccess(midiAccess);
-                },
+                    this._midiAccess = midiAccess;
+                }.bind(this),
                 function (error) {
                     console.error('Web MIDI initialization failed!', error);
                 }
@@ -16,45 +21,45 @@ define(['backbone', 'underscore'], function (Backbone, _) {
         }
     };
 
-    Midi.prototype = _.extend({
-        selectedIndex: null,
-        midiAccess: null,
-        actions: null,
+    Midi.prototype = {
+        _selectedIndex: null,
+        _midiAccess: null,
+        _actions: null,
+        _accessPromise: null,
+
         selectInput: function (index) {
-            if (this.selectedIndex) {
-                this.midiAccess.inputs()[this.selectedIndex].onmidimessage = undefined;
-                this.selectedIndex = null;
+            if (this._selectedIndex) {
+                this._midiAccess.inputs()[this._selectedIndex].onmidimessage = undefined;
+                this._selectedIndex = null;
             }
             // There seems to be a bug in the Web Midi implementation, that will cause the onmidimessage handler
             // to vanish, if theres no reference to the used input...
-            window.inputs = this.midiAccess.inputs();
-            var input = this.midiAccess.inputs()[index];
+            window.inputs = this._midiAccess.inputs();
+            var input = this._midiAccess.inputs()[index];
             if (!input) {
                 console.warn('Midi input #' + index + 'not found!');
                 return;
             }
             input.onmidimessage = this.onMessage.bind(this);
-            this.selectedIndex = index;
+            this._selectedIndex = index;
         },
+
         onMessage: function (event) {
             var data = event.data,
                 actionDescriptor = pad(data[0].toString(16), 0, 2) +
                     pad(data[1].toString(16), 0, 2) +
                     pad(data[2].toString(16), 0, 2),
-                action = this.actions[actionDescriptor];
+                action = this._actions[actionDescriptor];
             if (action) {
                 action(event);
             }
             console.log('Calling action handler for "' + actionDescriptor + '"');
         },
+
         registerAction: function (filter, action) {
-            this.actions[filter] = action;
-        },
-        setMidiAccess: function (midiAccess) {
-            this.midiAccess = midiAccess;
-            this.trigger('initialized');
+            this._actions[filter] = action;
         }
-    }, Backbone.Events);
+    };
 
     function pad(input, padSymbol, length) {
         var output = input;
